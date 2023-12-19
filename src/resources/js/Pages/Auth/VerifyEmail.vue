5fa2dc71f9e80459b94722ecf237f94a5e1fe9c2
Hiroshi Yamashita <yama01hiro@gmail.com>
2023-12-19 13:51:18 +0900

    :adhesive_bandage: vueファイルでリンクからページに飛べるようにimportの設定を修正した

--- src/resources/js/Pages/Auth/VerifyEmail.vue
+++ src/resources/js/Pages/Auth/VerifyEmail.vue
@@ -2,7 +2,7 @@
 import { computed } from 'vue';
 import GuestLayout from '@/Layouts/GuestLayout.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
-import { Head, Link, useForm } from '@inertiajs/vue3';
+import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
 
 const props = defineProps({
     status: String,
@@ -19,6 +19,7 @@ const verificationLinkSent = computed(() => props.status === 'verification-link-
 
 <template>
     <GuestLayout>
+
         <Head title="Email Verification" />
 
         <div class="mb-4 text-sm text-gray-600">
@@ -36,13 +37,9 @@ const verificationLinkSent = computed(() => props.status === 'verification-link-
                     Resend Verification Email
                 </PrimaryButton>
 
-                <Link
-                    :href="route('logout')"
-                    method="post"
-                    as="button"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >Log Out</Link
-                >
+                <Link :href="route('logout')" method="post" as="button"
+                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Log Out</Link>
             </div>
         </form>
     </GuestLayout>
