e4c9ed085d6439e16eb76b77939164f9cf0d0ced
Hiroshi Yamashita <yama01hiro@gmail.com>
2023-12-26 16:36:08 +0900

    :seedling: 店舗用のダミーデータを作成した

---
+++ src/database/factories/RestaurantFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Restaurant>
+ */
+class RestaurantFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => '',
+            'email' => $this->faker->unique()->safeEmail(),
+            'postal' => $this->faker->postcode,
+            'address' => $this->faker->prefecture . $this->faker->city . $this->faker->streetAddress,
+            'description' => '',
+            'prefecture_id' => $this->faker->numberBetween(1, 47),
+            'genre_id' => '',
+        ];
+    }
+}
---
+++ src/database/migrations/2023_12_26_165930_create_restaurants_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->char('postal', 7);
+            $table->string('address');
+            $table->string('description');
+            $table->string('restaurant_image')->nullable();
+            $table->foreignId('prefecture_id')->nullable()->constrained('prefectures')->onDelete('set null');
+            $table->foreignId('genre_id')->nullable()->constrained('genres')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
