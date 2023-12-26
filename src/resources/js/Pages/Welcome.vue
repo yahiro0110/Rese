0193ac1d9b40d7bbb41fb753569000bad17d0df6
Hiroshi Yamashita <yama01hiro@gmail.com>
2023-12-27 08:32:53 +0900

    :lipstick: ダッシュボードをホームに変更し、店舗一覧を表示するようにした

--- src/resources/js/Pages/Welcome.vue
+++ src/resources/js/Pages/Welcome.vue
@@ -15,9 +15,9 @@ defineProps({
     <div
         class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
         <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
-            <Link v-if="$page.props.auth.user" :href="route('dashboard')"
+            <Link v-if="$page.props.auth.user" :href="route('home')"
                 class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
-            Dashboard</Link>
+            Home</Link>
 
             <template v-else>
                 <Link :href="route('login')"
--- src/routes/web.php
+++ src/routes/web.php
@@ -31,9 +31,9 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/home', [RestaurantController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
