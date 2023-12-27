a87097e3256ac7c13d766df4d8da7f2012678965
Hiroshi Yamashita <yama01hiro@gmail.com>
2023-12-27 14:35:52 +0900

    :lipstick: 店舗情報の表示内容を変更した

--- src/resources/js/Pages/Restaurants/Index.vue
+++ src/resources/js/Pages/Restaurants/Index.vue
@@ -26,7 +26,7 @@
  * @template ユーザ一覧の表示、検索バー、ロール選択チェックボックス、フラッシュメッセージ、ページネーション
  */
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/inertia-vue3';
+import { Head, Link } from '@inertiajs/inertia-vue3';
 
 /**
  * コンポーネントのプロパティ定義。
@@ -56,53 +56,28 @@ const sushiImageUrl = '/storage/images/sushi.jpg';
                     <div class="p-6 text-gray-900">
                         <section class="text-gray-600 body-font">
                             <div class="container px-5 py-24 mx-auto">
+                                <div class="flex flex-wrap w-full mb-20">
+                                    <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
+                                        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
+                                            TMPさんの店舗情報</h1>
+                                        <div class="h-1 w-20 bg-indigo-500 rounded"></div>
+                                    </div>
+                                    <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">
+                                        この度は、私たちのサイトにご登録いただき、誠にありがとうございます。こちらのページでは、あなたが登録されたお店の情報を表示しております。お店の情報に関しましては、必要に応じて随時編集が可能です。お客様にとってより魅力的で正確な情報を提供するため、ご活用いただけますと幸いです。
+                                        何かご不明点やご質問がございましたら、お気軽にご連絡ください。</p>
+                                </div>
                                 <div class="flex flex-wrap -m-4">
-                                    <div class="p-4 md:w-1/3" v-for="restaurant in restaurants" :key="restaurant.id">
-                                        <div
-                                            class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                                            <img class="lg:h-48 md:h-36 w-full object-cover object-center"
-                                                :src="sushiImageUrl" alt="blog">
-                                            <div class="p-6">
-                                                <h2
-                                                    class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
-                                                    CATEGORY</h2>
-                                                <h2 class="title-font text-lg font-medium text-gray-900 mb-3">焼肉
-                                                </h2>
-                                                <h1 class="title-font text-lg font-medium text-gray-900 mb-3">{{
-                                                    restaurant.name }}
-                                                </h1>
-                                                <p class="leading-relaxed mb-3">{{ restaurant.description }}</p>
-                                                <div class="flex items-center flex-wrap ">
-                                                    <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn
-                                                        More
-                                                        <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor"
-                                                            stroke-width="2" fill="none" stroke-linecap="round"
-                                                            stroke-linejoin="round">
-                                                            <path d="M5 12h14"></path>
-                                                            <path d="M12 5l7 7-7 7"></path>
-                                                        </svg>
-                                                    </a>
-                                                    <span
-                                                        class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                                                        <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2"
-                                                            fill="none" stroke-linecap="round" stroke-linejoin="round"
-                                                            viewBox="0 0 24 24">
-                                                            <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                                                            <circle cx="12" cy="12" r="3"></circle>
-                                                        </svg>1.2K
-                                                    </span>
-                                                    <span
-                                                        class="text-gray-400 inline-flex items-center leading-none text-sm">
-                                                        <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2"
-                                                            fill="none" stroke-linecap="round" stroke-linejoin="round"
-                                                            viewBox="0 0 24 24">
-                                                            <path
-                                                                d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z">
-                                                            </path>
-                                                        </svg>6
-                                                    </span>
-                                                </div>
-                                            </div>
+                                    <div class="xl:w-1/4 md:w-1/2 p-4">
+                                        <div class="bg-gray-100 p-6 rounded-lg">
+                                            <img class="h-40 rounded w-full object-cover object-center mb-6"
+                                                src="https://dummyimage.com/720x400" alt="content">
+                                            <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">
+                                                店舗名</h3>
+                                            <h2 class="text-lg text-gray-900 font-medium title-font mb-4">仙人</h2>
+                                            <Link as="button"
+                                                class="flex ml-auto text-white bg-indigo-500 border-0 p-2 focus:outline-none hover:bg-indigo-600 rounded text-xs">
+                                            詳細
+                                            </Link>
                                         </div>
                                     </div>
                                 </div>
