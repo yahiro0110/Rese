4c74be7d91acf079795108511436a7c3618c855f
Hiroshi Yamashita <yama01hiro@gmail.com>
2024-01-06 14:50:39 +0900

    :lipstick: 検索用フォーム（プロトタイプ）の作成

--- src/resources/js/Pages/Home.vue
+++ src/resources/js/Pages/Home.vue
@@ -155,7 +155,44 @@ const detachRestaurant = (restaurant) => {
                     <div class="p-6 text-gray-900">
                         <FlashMessage />
                         <section class="text-gray-600 body-font" v-if="!selectedRestaurant">
-                            <div class="container px-5 py-24 mx-auto">
+                            <div class="container px-5 py-4 mx-auto">
+                                <div class="pb-6 sm:flex justify-end rounded-lg">
+                                    <!-- Select -->
+                                    <div class="relative">
+                                        <select data-hs-select='{
+                                                "placeholder": "Select option...",
+                                                "toggleTag": "<button type=\"button\"></button>",
+                                                "toggleClasses": "hs-select-disabled:pointer-events-none hs-select-disabled:opacity-50 relative py-3 px-4 pe-9 flex text-nowrap w-full cursor-pointer bg-white border border-gray-200 rounded-lg text-start text-sm focus:border-blue-500 focus:ring-blue-500 before:absolute before:inset-0 before:z-[1] dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600",
+                                                "dropdownClasses": "mt-2 z-50 w-full max-h-[300px] p-1 space-y-0.5 bg-white border border-gray-200 rounded-lg overflow-hidden overflow-y-auto dark:bg-slate-900 dark:border-gray-700",
+                                                "optionClasses": "py-2 px-4 w-full text-sm text-gray-800 cursor-pointer hover:bg-gray-100 rounded-lg focus:outline-none focus:bg-gray-100 dark:bg-slate-900 dark:hover:bg-slate-800 dark:text-gray-200 dark:focus:bg-slate-800",
+                                                "optionTemplate": "<div class=\"flex justify-between items-center w-full\"><span data-title></span><span class=\"hidden hs-selected:block\"><svg class=\"flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500\" xmlns=\"http:.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"/></svg></span></div>"
+                                            }' class="hidden">
+                                            <option value="">Choose</option>
+                                            <option>Name</option>
+                                            <option>Email address</option>
+                                            <option>Description</option>
+                                            <option>User ID</option>
+                                        </select>
+                                        <div class="absolute top-1/2 end-3 -translate-y-1/2">
+                                            <svg class="flex-shrink-0 w-3.5 h-3.5 text-gray-500 dark:text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                                <path d="m7 15 5 5 5-5" />
+                                                <path d="m7 9 5-5 5 5" />
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    <!-- End Select -->
+                                    <input type="text" class="py-3 px-4 pe-11 block w-1/6 border-gray-200 shadow-sm -mt-px -ms-px first:rounded-t-lg last:rounded-b-lg sm:first:rounded-s-lg sm:mt-0 sm:first:ms-0 sm:first:rounded-se-none sm:last:rounded-es-none sm:last:rounded-e-lg text-sm relative focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none">
+                                    <input type="text" class="py-3 px-4 pe-11 block w-1/6 border-gray-200 shadow-sm -mt-px -ms-px first:rounded-t-lg last:rounded-b-lg sm:first:rounded-s-lg sm:mt-0 sm:first:ms-0 sm:first:rounded-se-none sm:last:rounded-es-none sm:last:rounded-e-lg text-sm relative focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none">
+                                    <div class="relative max-w-xs w-full">
+                                        <div class="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-3.5">
+                                            <svg class="flex-shrink-0 w-4 h-4 text-gray-700" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                                <circle cx="11" cy="11" r="8" />
+                                                <path d="m21 21-4.3-4.3" />
+                                            </svg>
+                                        </div>
+                                        <input type="text" class="py-3 px-4 ps-11 block w-full border-gray-200 shadow-sm -mt-px -ms-px first:rounded-t-lg last:rounded-b-lg sm:first:rounded-s-lg sm:mt-0 sm:first:ms-0 sm:first:rounded-se-none sm:last:rounded-es-none sm:last:rounded-e-lg text-sm relative focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" placeholder="店舗名を入力してください" data-hs-overlay="#hs-pro-dnsm">
+                                    </div>
+                                </div>
                                 <div class="flex flex-wrap -m-4">
                                     <div class="p-4 md:w-1/3" v-for="restaurant in reactiveRestaurants" :key="restaurant.id">
                                         <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
