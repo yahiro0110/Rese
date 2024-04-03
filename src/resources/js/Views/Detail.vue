d1ace531db04c81f982d91c0244578c702415d88
Hiroshi Yamashita <yama01hiro@gmail.com>
2024-04-03 14:31:17 +0900

    :zap: ユーザロールによる口コミ機能の見直し ユーザのロールをもとに口コミ機能の制限を設けた。 - 店舗代表者・・・評価するボタンの非表示 - 管理者・・・・・削除ボタンの表示制御をIDからロール名称に変更

--- src/resources/js/Views/Detail.vue
+++ src/resources/js/Views/Detail.vue
@@ -59,11 +59,6 @@ const extractedReviews = computed(() => {
 const reviewCount = computed(() => extractedReviews.value.length);
 
 /**
- * 管理者ユーザのID。
- */
-const adminId = 1;
-
-/**
  * 日付をYYYY-MM-DD形式に変換する関数。
  *
  * @param {string} dateString - 変換する日付の文字列
@@ -222,6 +217,7 @@ const deleteReview = (id) => {
                     <h1 class="text-gray-900 text-3xl title-font font-medium mb-4">{{ restaurant.name }}
                     </h1>
                     <div class="flex mb-4">お店の概要</div>
+                    <p>{{ $page.props.loginUser.roles[0].name }}</p>
                     <p class="leading-relaxed mb-4">{{ restaurant.description }}</p>
                     <div class="flex border-t border-gray-200 py-2">
                         <span class="text-gray-500">住所</span>
@@ -237,7 +233,7 @@ const deleteReview = (id) => {
                     </div>
                     <div class="flex">
                         <button @click="openModal" class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">予約する</button>
-                        <button @click="openInputReviewForm" class="flex ml-4 text-white bg-orange-500 border-0 py-2 px-6 focus:outline-none hover:bg-orange-600 rounded">評価する</button>
+                        <button v-if="!$page.props.loginUser.roles.some(role => role.name === 'manager')" @click="openInputReviewForm" class="flex ml-4 text-white bg-orange-500 border-0 py-2 px-6 focus:outline-none hover:bg-orange-600 rounded">評価する</button>
                         <button @click="emitToggleLike(restaurant)" class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4" :class="{ 'bg-red-100': restaurant.liked }">
                             <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24" :class="{ 'text-red-500': restaurant.liked }">
                                 <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
@@ -341,7 +337,7 @@ const deleteReview = (id) => {
 
                         <div class="mt-1 text-right">
                             <button v-show="review.userId === $page.props.auth.user.id" @click="selectReview(review)" class="ml-auto mr-2 text-white text-sm bg-orange-500 border-0 p-2 focus:outline-none hover:bg-orange-600 rounded">編集</button>
-                            <button v-show="(review.userId === $page.props.auth.user.id) || ($page.props.auth.user.id === adminId)" @click="deleteReview(review.reviewId)" class="text-white text-sm bg-red-500 border-0 p-2 focus:outline-none hover:bg-red-600 rounded">削除</button>
+                            <button v-show="(review.userId === $page.props.auth.user.id) || ($page.props.loginUser.roles.some(role => role.name === 'admin'))" @click="deleteReview(review.reviewId)" class="text-white text-sm bg-red-500 border-0 p-2 focus:outline-none hover:bg-red-600 rounded">削除</button>
                         </div>
                     </div>
                 </div>
